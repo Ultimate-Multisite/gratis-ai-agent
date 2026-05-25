cf25629511a85d00d91f16ca9fe92874018f6955
David Stone <david@nnucomputerwhiz.com>
2026-05-25 12:01:25 -0600

    wip: separate curl_setopt calls for resource and CurlHandle types

--- includes/Core/Net/SafeFetcher.php
+++ includes/Core/Net/SafeFetcher.php
@@ -142,7 +142,10 @@ class SafeFetcher {
 					return;
 				}
 			}
-			if ( is_resource( $handle ) || $handle instanceof \CurlHandle ) {
+			if ( is_resource( $handle ) ) {
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- need raw cURL to set CURLOPT_RESOLVE; wp_remote_get does not expose this option
+				curl_setopt( $handle, CURLOPT_RESOLVE, [ $resolve_arg ] );
+			} elseif ( $handle instanceof \CurlHandle ) {
 				// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- need raw cURL to set CURLOPT_RESOLVE; wp_remote_get does not expose this option
 				curl_setopt( $handle, CURLOPT_RESOLVE, [ $resolve_arg ] );
 			}
